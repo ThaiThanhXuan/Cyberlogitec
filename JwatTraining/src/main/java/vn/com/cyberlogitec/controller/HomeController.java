e92f2d9ce1ec58f54933b28e90f5a9e2deaa0709
tvttvagree <langtuvodanh.mttg@gmail.com>
2020-05-01 21:01:42 +0700

    Add comment

--- JwatTraining/src/main/java/vn/com/cyberlogitec/controller/HomeController.java
+++ JwatTraining/src/main/java/vn/com/cyberlogitec/controller/HomeController.java
@@ -17,6 +17,11 @@ public class HomeController {
 		return new ModelAndView("home");
 	}
 	
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
 	@RequestMapping("/greeting")
 	public ModelAndView greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
 		return new ModelAndView("greeting");
