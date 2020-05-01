8f8e6c605fd97f7711cb4617759a51d5f89dffcd
thuantran-cyber <thuan.tran@cyberlogitec.com>
2020-05-01 20:58:04 +0700

    Add comment

--- JwatTraining/src/main/java/vn/com/cyberlogitec/controller/HomeController.java
+++ JwatTraining/src/main/java/vn/com/cyberlogitec/controller/HomeController.java
@@ -23,6 +23,11 @@ public class HomeController {
 		return new ModelAndView("home");
 	}
 	
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
 	@RequestMapping("/greeting")
 	public ModelAndView greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
 		return new ModelAndView("greeting");
