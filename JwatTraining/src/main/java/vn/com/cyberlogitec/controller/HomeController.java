bbf9da386174612a599d0897efd568f49e2db966
thuantran-cyber <thuan.tran@cyberlogitec.com>
2020-05-04 16:15:53 +0700

    Test

--- JwatTraining/src/main/java/vn/com/cyberlogitec/controller/HomeController.java
+++ JwatTraining/src/main/java/vn/com/cyberlogitec/controller/HomeController.java
@@ -12,15 +12,24 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+	private String test() {
+		return new String("1");
+	}
+	
 	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
+	public ModelAndView test(HttpServletResponse response) throws IOException {
+		String a = test();
+		String b = test();
+		if (a == b) {
+			System.out.println("Test");
+		}
 		return new ModelAndView("home");
 	}
 	
 	/**
 	 * 
 	 * @param name
-	 * @return
+	 * @return ModelAndView
 	 */
 	@RequestMapping("/greeting")
 	public ModelAndView greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
