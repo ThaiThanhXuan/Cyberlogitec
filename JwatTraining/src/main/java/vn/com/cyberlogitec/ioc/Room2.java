223b3db7cf35bff018ba29b9fd7d53903a28e58b
ThinhDDo <doduythinhfa.1999@gmail.com>
2020-05-05 16:08:18 +0700

    IoC example with setter, constructor and Field.

---
+++ JwatTraining/src/main/java/vn/com/cyberlogitec/ioc/Room2.java
@@ -0,0 +1,28 @@
+package vn.com.cyberlogitec.ioc;
+
+public class Room2 implements IRoom {
+
+	private IAnimal animal;
+	
+	public Room2() {}
+	
+	public Room2(IAnimal animal) {
+		this.animal = animal;
+	}
+	
+	@Override
+	public IAnimal getAnimal() {
+		return animal;
+	}
+
+	@Override
+	public void setAnimal(IAnimal animal) {
+		this.animal = animal;
+	}
+	
+	@Override
+	public void getRoom() {
+		System.out.println("Room 2");
+	}
+
+}
